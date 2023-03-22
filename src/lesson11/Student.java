13fb3d3d1ba0f89c81048c8d8a222cd81de30ee9
¨Viachaslau_Mukhin¨ <viachaslau.mukhin@expert-soft.by>
2023-03-22 20:33:53 +0300

    Add lesson 12 example

--- src/lesson11/Student.java
+++ src/lesson11/Student.java
@@ -1,5 +1,7 @@
 package lesson11;
 
+import java.util.Objects;
+
 public class Student
 {
     private String name;
@@ -55,5 +57,20 @@ public class Student
                 ", group='" + group + '\'' +
                 ", mark=" + mark +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Float.compare(student.mark, mark) == 0 && Objects.equals(name, student.name) && Objects.equals(group, student.group);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(name, group, mark);
     }
 }
